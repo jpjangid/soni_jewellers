6d431dcbe727f1fc39d630d018bdd58476d6e712
kinal kukda <mohsin@webanix.in>
2023-01-10 00:01:52 +0530

    commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -74,7 +74,6 @@ import { LoginComponent } from './components/auth/login/login.component';
 import { ItemMasterComponent } from './components/forms/item-master/item-master.component';
 import { BillGenerateComponent } from './components/forms/bill-generate/bill-generate.component';
 import { RegisteredUserComponent } from './components/common/registered-user/registered-user.component';
-import { CouponComponent } from './components/forms/coupon/coupon.component';
 // import { CustomerFormComponent } from './components/custom/supplier-components/customer-form/customer-form.component';
 // import { SupplierchangerequestComponent } from './components/custom/supplier-components/supplierchangerequest/supplierchangerequest.component';
 // import { EnquiryComparisonByIdComponent } from './components/custom/user-components/enquiry-comparison-by-id/enquiry-comparison-by-id.component';
@@ -128,7 +127,6 @@ import { CouponComponent } from './components/forms/coupon/coupon.component';
     SplitPipe,
     BillGenerateComponent,
     RegisteredUserComponent,
-    CouponComponent,
     // CustomerFormComponent,
     // SupplierchangerequestComponent,
     // EnquiryComparisonByIdComponent,
